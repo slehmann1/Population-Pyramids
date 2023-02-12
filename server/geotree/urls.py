725ef57ab8f424074553a5524f11ae0bca56bb60
slehmann1 <samueljlehmann@gmail.com>
2023-02-11 21:40:30 -0800

    Convert to Django app

--- server/geotree/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'geotree'
-urlpatterns = [
-    path('', views.geotree, name='geo_select'),
-    path('geo/<str:geoname>/', views.DetailView.as_view(), name = 'details'),
-    path('geo/', views.geo_selected, name='geo_selected')
-]
--- server/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from geotree import views
-
-class ViewsTests(TestCase):
-    def test_round_max(self):
-        self.assertEqual(views.DetailView.round_max(1025), 1100)
-        self.assertEqual(views.DetailView.round_max(23335), 24000)
-        self.assertEqual(views.DetailView.round_max(33335), 40000)
-        self.assertEqual(views.DetailView.round_max(92), 100)
